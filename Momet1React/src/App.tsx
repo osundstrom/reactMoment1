6e984d5596ac5b10af15075d521a7aa92a2ff06f
osundstrom <oskar.sundstrom123@gmail.com>
2025-01-20 23:42:53 +0100

    first

---
+++ Momet1React/src/App.tsx
@@ -0,0 +1,12 @@
+import Hobby from "./components/Hobby"
+
+function App() {
+  return (
+    <>
+        <h1>Moment 1</h1>
+        <Hobby/>
+    </>
+  )
+}
+
+export default App
---
+++ Momet1React/src/components/Header.tsx
@@ -0,0 +1,24 @@
+function Header() {
+
+    const test = "Oskar";
+
+    return (
+        <>
+
+    <div>
+        <h1 className="testH1">{10*5}</h1>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    </div>
+
+
+
+
+    </>   
+    )
+}
+
+export default Header;
